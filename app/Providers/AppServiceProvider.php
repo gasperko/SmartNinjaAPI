6271b2a42998ae42f1d703b5c1caa36d60497756
Miha Susnik <mihec.susnik@gmail.com>
2015-05-15 00:26:05 +0200

    Added CORS.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -12,6 +12,18 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        /** @var \Illuminate\Http\Request $request */
+        $request = $this->app->make('request');
+
+        if(isset($_SERVER['HTTP_ORIGIN'])) header('Access-Control-Allow-Origin: ' . $_SERVER['HTTP_ORIGIN']);
+        header('Access-Control-Allow-Methods: POST, GET, OPTIONS');
+        header('Access-Control-Allow-Headers: Content-Type, X-Auth-Token, Origin, Accept');
+        header('Access-Control-Allow-Credentials: true');
+
+        if($request->isMethod('OPTIONS')) {
+            $this->app->options($request->path(), function(){
+                return response('OK', 200);
+            });
+        }
     }
 }
